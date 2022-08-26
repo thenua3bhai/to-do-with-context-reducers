7865e97e400bc02a567d02d9e6962e9373b20507
Aditya Singh <adityasingh11156@gmail.com>
2022-08-26 13:36:08 +0530

    2nd commit

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,3 +3,4 @@
 //Note:  useEffect() React compunnets ke alawa hooks,contexts and reducers m bhi likha ja skta h and wese hi
 //run hoga jese React componnets m hota h.. componnet render ke bad jo array m 2nd arg passed h uske change
 // hote hi
+// new change
